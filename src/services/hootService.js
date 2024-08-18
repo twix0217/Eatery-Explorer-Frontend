6597c3123925756b5a820b33d5bee650d88ae8b0
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-08-18 14:30:48 +0300

    created

---
+++ src/services/hootService.js
@@ -0,0 +1,39 @@
+const BASE_URL = `${import.meta.env.VITE_BACKEND_URL}/hoots`;
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const show = async (hootId) => {
+  try {
+    const res = await fetch(`${BASE_URL}/${hootId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    });
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const create = async (formData) => {
+  const options = {
+    method: 'POST',
+    headers: {
+              Authorization: `Bearer ${localStorage.getItem('token')}`,
+              'Content-Type': 'application/json',
+            },
+    body: JSON.stringify(formData)
+  }
+  const res = await fetch(BASE_URL, options)
+
+  return res.json()
+}
+
+export default { index, show, create };
