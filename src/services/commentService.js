658bf8e3dc6ab4445fdac11174bfa62340e63663
Mujtaba_Sultan <b.mojtabaali@gmail.com>
2024-08-20 18:24:28 +0300

    added delete to food comments

--- src/services/commentService.js
+++ src/services/commentService.js
@@ -55,6 +55,24 @@ const deleteC = async (restaurantId, commentId) => {
   }
 };
 
+
+const deleteFoodComment = async (restaurantId,foodId, commentId) => {
+  try {
+    const data = {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    };
+
+    const res = await fetch(`${BASE_URL}/${restaurantId}/menu/${foodId}/comments/${commentId}`, data);
+
+    return res.json();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
 export default {
-  create,createFC,deleteC
+  create,createFC,deleteC,deleteFoodComment
 };
